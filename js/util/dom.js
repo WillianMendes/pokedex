cf0f7914a34ab2ee187ad9d135fc88fd9baaef06
Willian Mendes <willianmendesbr@gmail.com>
2021-04-24 19:26:14 -0300

    Create functions util from dom

---
+++ js/util/dom.js
@@ -0,0 +1,20 @@
+const getElementsByClass = (className) =>
+  document.getElementsByClassName(className);
+
+const getElementById = (idName) => document.getElementById(idName);
+
+const insertPokeAvatar = (id, urlImg) => {
+  const pokeImg = getElementById(id);
+  pokeImg.style.backgroundImage = `url('${urlImg}')`;
+};
+
+const createPokeCard = (name) => `<div class="poke-card">
+    <div id="${name}_id" class="poke-img-card"></div>
+    <div class="poke-body-card">
+      <div class="poke-title">
+        <span>${name}</span>
+      </div>
+    </div>
+  </div>`;
+
+export { getElementById, getElementsByClass, createPokeCard, insertPokeAvatar };
