a996d12ce56df69dfb9901bea595347647d2de3c
Willian Mendes <willianmendesbr@gmail.com>
2021-04-28 12:31:36 -0300

    template poke-card created to js

---
+++ template/poke-card.js
@@ -0,0 +1,15 @@
+const templateTypes = (types) =>
+  types.map((type) => `<span class='poke-card-type'>${type.name}</span>`);
+
+const template = (pokemon) => `<div id="${pokemon.id}" class="poke-card">
+    <div class="poke-card-info">
+      <h2>${pokemon.name}</h2>
+      ${templateTypes(pokemon.types)}
+      <span class="poke-card-id">${pokemon.id}</span>
+    </div>
+    <div class="poke-card-img">
+      <img src="${pokemon.src}" alt="${pokemon.name}">
+    </div>
+  </div>`;
+
+export default template;
